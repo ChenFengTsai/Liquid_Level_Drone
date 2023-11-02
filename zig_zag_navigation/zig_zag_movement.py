855e59c0d073e59f31c3f31615173b62e4e95e83
Nicolas Ferreira <nicoferreira95@gmail.com>
2023-11-01 20:10:16 -0500

    Implementation of zigzag movement with navigation

--- zig_zag_navigation/zig_zag_movement.py
+++ zig_zag_navigation/zig_zag_movement.py
@@ -2,22 +2,24 @@ import time
 from djitellopy import Tello
 
 # Initialize the Tello drone connection
-tello = Tello()
+tello = Tello("192.168.87.41")
 tello.connect()
 tello.takeoff()
 
-def lawnmower_pattern(distance=100, height=20, wait_time=3, segments=4):
+def lawnmower_pattern(distance=100, height=30, wait_time=3, segments=4, direction="left"):
     segment_distance = distance // segments
     for _ in range(segments):
-        tello.move_forward(segment_distance)
+        if direction == "left":
+            tello.move_left(segment_distance)
+        else:  # direction == "right"
+            tello.move_right(segment_distance)
         time.sleep(wait_time)
 
-def zigzag_movement(patterns=3, height=20):
+def zigzag_movement(patterns=3, distance=100, height=30, segments=4):
+    directions = ["left", "right", "left"]  # Starting from bottom right, as specified
     for i in range(patterns):
-        if i % 2 == 0:  # Even pattern (0-indexed)
-            lawnmower_pattern(height=height)
-        else:  # Odd pattern
-            lawnmower_pattern(height=height)
+        lawnmower_pattern(distance=distance, height=height, segments=segments, direction=directions[i])
+        if i != patterns - 1:  # Don't move up after the last pattern
             tello.move_up(height)
 
 # Perform the movement
